af8de33cbcc6ec8cbd81d2c2385fa750f92c680c
Nick <nikitakrm87@gmail.com>
2020-01-27 10:46:05 +0200

    added forOf

--- js-hw-02/task-6.js
+++ js-hw-02/task-6.js
@@ -15,13 +15,11 @@ while (true) {
         console.log('Было введено не число, пропускаем текущую итерацию!');
         continue;
     }
-
     numbers.push(input);
+}
 
-    for (let i = 0; i < numbers.length; i + 1) {
-        total += input;
-        break;
-    }
+for (const number of numbers) {
+    total += number;
 }
 console.log(`Вводимые числа: ${numbers} и общая сумма чисел равна ${total}.`);
 alert(`Общая сумма чисел равна ${total}.`);
